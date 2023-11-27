6837429ff5dcb6040f1f0a9edc4116fbdff3a78c
Excis3 <info@Excis3.be>
2023-11-27 10:02:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "under construction"
 
 @app.route("/excis3")
-def hello():
+def poc():
     return "Subdomain Takeover PoC By <a href=https://hackerone.com/excis3>Excis3</a>"
 
 if __name__ == '__main__':
