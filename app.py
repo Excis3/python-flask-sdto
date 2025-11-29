d54dbbf3accbfe2a0049c6ec9424088c5b2fb286
Excis3 <info@Excis3.be>
2025-11-29 10:17:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    html = "Site under construction <br> <!--Subdomain takeover regarding the Bugbounty program of this asset. more info on /excis3"
-    return html
+    return "Site under construction <br> <!--Subdomain takeover regarding the Bugbounty program of this asset. more info on /excis3"
 
 @app.route('/excis3')
 def poc():
-    html = "Subdomain Takeover PoC By <a href=https://hackerone.com/excis3>Excis3</a>"
-    return html
+    return "Subdomain Takeover PoC By <a href=https://hackerone.com/excis3>Excis3</a>"
 
 if __name__ == '__main__':
     app.run(debug=True)
